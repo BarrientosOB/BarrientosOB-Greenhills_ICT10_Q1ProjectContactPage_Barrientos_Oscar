a16db33ea9258d5d4eff06144f4c00c13c9b8476
BarrientosOB <barrientos.op@obmontessori.edu.ph>
2025-09-30 18:08:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyscript import display
+
+
+page_name = 'Contact Us' # String
+year_established = 2022 # Integer
+owner_name = 'Oscar Barrientos' # String
+business_hours = 'Opening Hours: 6 A.M to 7 P.M '
+
+
+
+
+display(page_name, target="div1" )
+display(business_hours, target="div4")
+
+
+
+
+
